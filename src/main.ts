72b14c096a1573cb90e990881b097d4d8dc10eb4
EdgarBM <edgar@develogy.mx>
2025-03-01 19:05:33 -0600

    feat(app): add login with firebase

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap"
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDoFHLlMFXy5bjZhyyjPUB17I0QAUaHGQA",
+    authDomain: "curso-vue-88716.firebaseapp.com",
+    projectId: "curso-vue-88716",
+    storageBucket: "curso-vue-88716.firebasestorage.app",
+    messagingSenderId: "1011976796417",
+    appId: "1:1011976796417:web:5653307b50d3284867c300",
+    measurementId: "G-QCR80PHE5F"
+};
+
+initializeApp(firebaseConfig);
 createApp(App).use(router).mount('#app')
---
+++ src/views/FirebaseView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                v-model="email"
+                >            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1"
+                v-model="password"
+                >
+            </div>
+            <button type="submit" class="btn btn-primary mt-2" @click.prevent="authUser">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import {ref, Ref} from 'vue'
+    import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
+    let email = ref("")
+    let password = ref("")
+
+    const authUser = () => {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email.value, password.value).then(() => {
+            alert("User signed in successfully!");
+        })
+        .catch((error) => {
+            alert(error.message)
+        });
+    }
+</script>
